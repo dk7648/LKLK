481ecda10c78445f9631c313308fffc974722988
dk7648 <dk7648@korea.ac.kr>
2021-07-04 19:38:45 +0900

    django course 29 commit

--- pragmatic/settings.py
+++ pragmatic/settings.py
@@ -147,3 +147,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = reverse_lazy('accountapp:hello_world')
 LOGOUT_REDIRECT_URL = reverse_lazy('accountapp:login')
+
+#인터넷 주소창에 media에 접근하기 위한 경로
+MEDIA_URL = '/media/'
+
+#파일을 올리면 저장되는 공간
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
